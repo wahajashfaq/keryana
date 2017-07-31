089e1451a4f0478ec601c8f2c6b0b4c735faf69f
wahajashfaq <wahajashfaq2013@outlook.com>
2017-08-01 01:32:38 +0500

    category page done
    xlnxojx0pdks

--- application/views/category.php
+++ application/views/category.php
@@ -14,13 +14,129 @@
                 <a><div class="Maincat sub"><i class="fa fa-angle-right" aria-hidden="true"></i><span>Poha/Chidwa</span></div></a>
                 <a><div class="Maincat sub"><i class="fa fa-angle-right" aria-hidden="true"></i><span>Upma</span></div></a>
             </div>
-            <div class="cbox" style="margin-top:0px">
+            <div class="cbox" style="margin-top:10px">
                 <div class="cbox1">Refine</div>
                 <div class="head">Price</div>
+                <div class="check">
+                <input  type="checkbox" id="pr1" /> 
+                <label for="pr1">Under Rs 50</label>
+                </div>
+                <div class="check">
+                <input  type="checkbox" id="pr2" /> 
+                <label for="pr2">Rs 50 to Rs 100</label>
+                </div>
+                <div class="check">
+                <input  type="checkbox" id="pr3" /> 
+                <label for="pr3">Rs 100 to Rs 500</label>
+                </div>
+                <div class="check">
+                <input  type="checkbox" id="pr4" /> 
+                <label for="pr4">Rs 500 to Rs 1000</label>
+                </div>
+                <div class="check">
+                <input  type="checkbox" id="pr5" /> 
+                <label for="pr5">Above Rs 1000</label>
+                </div>
+                <div class="head">Brands</div>
+                <div class="check">
+                <input  type="checkbox" id="br1" /> 
+                <label for="br1">Nestle</label>
+                </div>
+                <div class="check">
+                <input  type="checkbox" id="br2" /> 
+                <label for="br2">Cocacola</label>
+                </div>
+                <div class="check">
+                <input  type="checkbox" id="br3" /> 
+                <label for="br3">Lipton</label>
+                </div>
+                <div class="check">
+                <input  type="checkbox" id="br4" /> 
+                <label for="br4">Tapal</label>
+                </div>
+                <div class="check">
+                <input  type="checkbox" id="br5" /> 
+                <label for="br5">Uniliver</label>
+                </div>
+                <div class="head">Weights</div>
+                <div class="check">
+                <input  type="checkbox" id="Wr1" /> 
+                <label for="Wr1">100 g</label>
+                </div>
+                <div class="check">
+                <input  type="checkbox" id="Wr2" /> 
+                <label for="Wr2">500 g</label>
+                </div>
+                <div class="check">
+                <input  type="checkbox" id="Wr3" /> 
+                <label for="Wr3">1 Kg</label>
+                </div>
+                <div class="check">
+                <input  type="checkbox" id="Wr4" /> 
+                <label for="Wr4">5 Kg</label>
+                </div>
+                <div class="check">
+                <input  type="checkbox" id="Wr5" /> 
+                <label for="Wr5">20 Kg</label>
+                </div>
             </div>
         </div>
         <div class="col-md-9">
-            <img class="cbanner" src="<?php echo base_url('assets/images/home_slider_image_1.jpg') ?>">
+            <img class="cbanner" src="<?php echo base_url('assets/images/home_slider_image_1.jpg') ?>"><br><br>
+            <div class="row">
+             <div class="col-md-3 product" style="padding-left:0px;padding-right:0px">
+              <div class="discount" style="margin-left:10px"><center>19% <span>OFF</span></center></div><br><br>
+              <center><div class="slider3-label"><img src="<?php echo base_url('assets/images/product.jpg') ?>" style="width:150px;height:150px">
+                <br><br><div>Product name</div>
+              </div><br>
+              <button class="qview">QUICK VIEW</button>
+              <div class="qfix">5 Kg</div>
+              <br>
+              <span class="oprice">Rs 1000</span><span class="nprice">Rs 500</span><br><br>
+              <div class="qmang"><i class="fa fa-minus" aria-hidden="true"></i><span class="qval">0</span><i class="fa fa-plus" aria-hidden="true"></i></div>
+              <button class="addbtn"><img src="<?php echo base_url('assets/images/addbicon.png') ?>"><span>Add</span></button>
+            </center>
+            </div>
+             <div class="col-md-3 product" style="padding-left:0px;padding-right:0px">
+              <div class="discount" style="margin-left:10px"><center>19% <span>OFF</span></center></div><br><br>
+              <center><div class="slider3-label"><img src="<?php echo base_url('assets/images/product.jpg') ?>" style="width:150px;height:150px">
+                <br><br><div>Product name</div>
+              </div><br>
+              <button class="qview">QUICK VIEW</button>
+              <div class="qfix">5 Kg</div>
+              <br>
+              <span class="oprice">Rs 1000</span><span class="nprice">Rs 500</span><br><br>
+              <div class="qmang"><i class="fa fa-minus" aria-hidden="true"></i><span class="qval">0</span><i class="fa fa-plus" aria-hidden="true"></i></div>
+              <button class="addbtn"><img src="<?php echo base_url('assets/images/addbicon.png') ?>"><span>Add</span></button>
+            </center>
+            </div>
+            <div class="col-md-3 product" style="padding-left:0px;padding-right:0px">
+              <div class="discount" style="margin-left:10px"><center>19% <span>OFF</span></center></div><br><br>
+              <center><div class="slider3-label"><img src="<?php echo base_url('assets/images/product.jpg') ?>" style="width:150px;height:150px">
+                <br><br><div>Product name</div>
+              </div><br>
+              <button class="qview">QUICK VIEW</button>
+              <div class="qfix">5 Kg</div>
+              <br>
+              <span class="oprice">Rs 1000</span><span class="nprice">Rs 500</span><br><br>
+              <div class="qmang"><i class="fa fa-minus" aria-hidden="true"></i><span class="qval">0</span><i class="fa fa-plus" aria-hidden="true"></i></div>
+              <button class="addbtn"><img src="<?php echo base_url('assets/images/addbicon.png') ?>"><span>Add</span></button>
+            </center>
+            </div>
+            <div class="col-md-3 product" style="padding-left:0px;padding-right:0px">
+              <div class="discount" style="margin-left:10px"><center>19% <span>OFF</span></center></div><br><br>
+              <center><div class="slider3-label"><img src="<?php echo base_url('assets/images/product.jpg') ?>" style="width:150px;height:150px">
+                <br><br><div>Product name</div>
+              </div><br>
+              <button class="qview">QUICK VIEW</button>
+              <div class="qfix">5 Kg</div>
+              <br>
+              <span class="oprice">Rs 1000</span><span class="nprice">Rs 500</span><br><br>
+              <div class="qmang"><i class="fa fa-minus" aria-hidden="true"></i><span class="qval">0</span><i class="fa fa-plus" aria-hidden="true"></i></div>
+              <button class="addbtn"><img src="<?php echo base_url('assets/images/addbicon.png') ?>"><span>Add</span></button>
+            </center>
+            </div>
+            </div>
         </div>
     </div>
 </div>
